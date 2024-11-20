5c843344e7db86d86e3b5fa64dee2ba88d1c65cc
Goon-ner <96110407+Goon-ner@users.noreply.github.com>
2024-11-20 18:14:02 +0300

    Update DataRetriever.java

--- app/src/main/java/com/example/movie4/DataRetriever.java
+++ app/src/main/java/com/example/movie4/DataRetriever.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class DataRetriever {
-    private static final String BASE_URL = "http://192.168.56.1:8080/";
+    private static final String BASE_URL = "http://localhost:8080/";
     public ApiService apiService;
 
     public DataRetriever() {
